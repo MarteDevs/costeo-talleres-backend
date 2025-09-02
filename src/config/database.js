2ed070b3eec0857db4d65f5f03a2b64c7f82506e
Marco Rodrigo Polo Silva <120402077+MarteDevs@users.noreply.github.com>
2025-09-02 08:12:57 -0500

    Update database.js

--- src/config/database.js
+++ src/config/database.js
@@ -6,15 +6,16 @@ dotenv.config();
 
 // Configurar el pool de conexiones usando las variables de entorno
 const pool = createPool({
-    host: process.env.MYSQLHOST, // Usa el nombre de la variable de Railway
-    user: process.env.MYSQLUSER,
-    password: process.env.MYSQLPASSWORD,
-    database: process.env.MYSQLDATABASE,
-    port: process.env.MYSQLPORT,
-    database: process.env.DB_NAME,
-    waitForConnections: true,
-    connectionLimit: 10,
-    queueLimit: 0
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0
+
+
 });
 
 // Función para verificar la conexión
